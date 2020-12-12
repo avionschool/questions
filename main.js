aadd1beaf69bcc753a51f50f96c4449fbe4b9225
Jerickc Camacho <jerickcamacho42@gmail.com>
2020-12-12 11:35:53 +0800

    task-1 create question constructor

---
+++ main.js
@@ -0,0 +1,6 @@
+let Question = function(question, answer1, answer2, correctAnswer) {
+    this.question = question; 
+    this.answer1 = answer1; 
+    this.answer2 = answer2; 
+    this.correctAnswer = correctAnswer; 
+}; 
