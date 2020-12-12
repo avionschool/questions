f3985aeef949141b8feea63a757c960aa9c5264e
Enrique Ferrer <emferrer4@gmail.com>
2020-12-12 12:30:40 +0800

    task-5 add prompt function

--- main.js
+++ main.js
@@ -11,9 +11,10 @@ let Question = function(question, answer, correctAnswer) {
     }
 }; 
 
-
 var question1 = new Question("How many members are there in TWICE?", ["7","8","9"], 2);
 var question2 = new Question("Who is the main dancer in TWICE?", ["Momo","Dahyun","Nayeon"], 0);
 var question3 = new Question("What year did TWICE debut?", ["2014","2015","2016"], 1);
 
 let questionsArray = [question1, question2, question3];
+
+prompt(question1.askQuestion());
