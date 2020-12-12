6e00d4849168a26e8ddb5d4c3ec1eda96101a8fe
Jerickc Camacho <jerickcamacho42@gmail.com>
2020-12-12 11:46:38 +0800

    task-1 apply changes

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-let Question = function(question, answer1, answer2, correctAnswer) {
+let Question = function(question, answer1, correctAnswer) {
     this.question = question; 
-    this.answer1 = answer1; 
-    this.answer2 = answer2; 
+    this.answer1 = answer1;  
     this.correctAnswer = correctAnswer; 
 }; 
