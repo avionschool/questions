439fe8cfc1dd99df86cb49491896635192847b5a
rookiemonkey <kevinroirigorbasina@pm.me>
2020-12-12 13:03:29 +0800

    task-7 display the next random question so that the game never ends

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-let Question = function(question, answer, correctAnswer) {
-    this.question = question; 
-    this.answer = answer;  
-    this.correctAnswer = correctAnswer; 
-    this.askQuestion = function() {
-        var randomNumber = Math.floor(Math.random()*3)
+let Question = function (question, answer, correctAnswer) {
+    this.question = question;
+    this.answer = answer;
+    this.correctAnswer = correctAnswer;
+    this.askQuestion = function () {
+        var randomNumber = Math.floor(Math.random() * 3)
         console.log(randomNumber)
         questionsArray[randomNumber]
         console.log(this.question)
@@ -16,14 +16,19 @@ let Question = function(question, answer, correctAnswer) {
             console.log('Incorrect Answer');
         }
     }
-}; 
+};
 
+let isGameRunning = true;
+let questionNumber = 0;
+var question1 = new Question("How many members are there in TWICE?", ["7", "8", "9"], 2);
+var question2 = new Question("Who is the main dancer in TWICE?", ["Momo", "Dahyun", "Nayeon"], 0);
+var question3 = new Question("What year did TWICE debut?", ["2014", "2015", "2016"], 1);
 let questionsArray = [question1, question2, question3];
-var question1 = new Question("How many members are there in TWICE?", ["7","8","9"], 2);
-var question2 = new Question("Who is the main dancer in TWICE?", ["Momo","Dahyun","Nayeon"], 0);
-var question3 = new Question("What year did TWICE debut?", ["2014","2015","2016"], 1);
 
-(function start(){
-    let tempAnswer = prompt(question1.askQuestion());
-    question1.checkAnswer(tempAnswer);
+(function start() {
+    while (isGameRunning) {
+        const currentQuestion = questionsArray[questionNumber];
+        let tempAnswer = prompt(currentQuestion.askQuestion());
+        currentQuestion.checkAnswer(tempAnswer);
+    }
 })();
